a074c4304b93096f5820932287f315a3ae2f191c
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-03-20 21:15:27 -0400

    Initial Commit

---
+++ WhatChaUp2/WhatChaUp2App.swift
@@ -0,0 +1,17 @@
+//
+//  WhatChaUp2App.swift
+//  WhatChaUp2
+//
+//  Created by Vivian Phung on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatChaUp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
